eb9b4063d1c0220ead7f7dfcaf5aea66d1a6911e
paulpellom@gmail.com <paulpellom@gmail.com>
2018-02-06 11:38:14 -0500

    Text epdate

--- gamelogic.js
+++ gamelogic.js
@@ -2,20 +2,22 @@ $('body').css("background-color:", "yellow");
 $(document).ready(function () {
 
 
-    $('body').html("yo is this bull working boi").append;
-    $('body').css('background-color', 'rgb(186, 243, 216)');
-    
+    $('p').html("blackjack Table").append;
+    $('p').css("color", 'rgba(123, 25, 87, 0.75)');
+    $('p').css("font-family", "Cookie");
+    $('p').css("font-size", "75px");
+    $
 
 
-
-    
     function randomScore(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min; 
-      } 
-      console.log("Your card has the value of " + randomScore(1, 52));
-      
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    console.log("Your card has the value of " + randomScore(1, 52));
+
+
+
 
 
 
@@ -24,3 +26,4 @@ $(document).ready(function () {
 
 
 })
+//Text colors: Berkshire+Swash|Cookie|Damion|Gloria+Hallelujah|Indie+Flower|Pacifico|Satisfy"
