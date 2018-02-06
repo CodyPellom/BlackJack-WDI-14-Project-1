bc2db74998c6cd2c8d6197bd0d89e13ed9d912b6
paulpellom@gmail.com <paulpellom@gmail.com>
2018-02-06 10:53:29 -0500

    Logic Beginning

--- gamelogic.js
+++ gamelogic.js
@@ -1,10 +1,26 @@
 $('body').css("background-color:", "yellow");
-$(Document).ready(function()) {
+$(document).ready(function () {
 
 
-$('body').html("yo is this bull working boi").append;
-${'body'}.css('background-color:', 'blue');
+    $('body').html("yo is this bull working boi").append;
+    $('body').css('background-color', 'rgb(186, 243, 216)');
+    
+
 
 
     
-}
+    function randomScore(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min; 
+      } 
+      console.log("Your card has the value of " + randomScore(1, 52));
+      
+
+
+
+
+
+
+
+})
